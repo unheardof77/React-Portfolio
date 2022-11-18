66b0ed2f81a2b80ef56ff3769cf984ae9bec0d29
Morgan Tolman <morgan.tolman04@gmail.com>
2022-11-17 23:02:46 -0700

    Styled biography section.  Added mobile compatibility for bio section.  Safe revert point.

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -20,27 +20,29 @@ export default function Home() {
 
     return (
         <section>
-            <article>
-                <section>
-                    <h2>Biography</h2>
-                    <img src={image} alt='Morgan Tolman' />
-                </section>
-                <section>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid corporis minima, error repellat nemo illo fugiat, maiores dolor iste deleniti, itaque aperiam fuga molestias earum. Excepturi recusandae tenetur atque minus.</p>
-                </section>
-            </article>
-
-            <section>
-                <form ref={formRef} onSubmit={(event)=>formSubmitHandler(event)}>
-                    <input name='email'  placeholder='Your Email'/>
-                    <input name='subject' placeholder='Subject' />
-                    <textarea name='message' placeholder='Your Message' />
-                    <button type='submit'>Send Email</button>
-                </form>
-                <article>
-                    <p></p>
-                    <a></a>
+            <section className='flex flex-col items-center '>
+                <article className='flex md:flex-row flex-col md:justify-center items-center w-full sm:w-4/6 bg-[rgb(21,41,78)]'>
+                    <section className='  w-full md:w-5/12 '>
+                        <h2>Biography</h2>
+                        <img className='rounded-full' src={image} alt='Morgan Tolman' />
+                    </section>
+                    <section className=' w-full md:w-5/12'>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid corporis minima, error repellat nemo illo fugiat, maiores dolor iste deleniti, itaque aperiam fuga molestias earum. Excepturi recusandae tenetur atque minus.</p>
+                    </section>
                 </article>
+
+                <section className='flex justify-center w-4/6 bg-[rgb(21,41,78)]'>
+                    <form ref={formRef} onSubmit={(event)=>formSubmitHandler(event)}>
+                        <input name='email'  placeholder='Your Email'/>
+                        <input name='subject' placeholder='Subject' />
+                        <textarea name='message' placeholder='Your Message' />
+                        <button type='submit'>Send Email</button>
+                    </form>
+                    <article>
+                        <p></p>
+                        <a></a>
+                    </article>
+                </section>
             </section>
         </section>
     );
